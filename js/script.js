b4768aca824496b7508ff11d60cbad60211c1d84
Gui Rodrigues <gui.sevening@gmail.com>
2016-10-20 22:27:38 +0200

    Rajout de JS + Correction de la taille des div dans la navbar

--- js/script.js
+++ js/script.js
@@ -32,14 +32,14 @@ moveBackground();
 $(window).scroll(function() {
 	if($("#biglogo").visible(true)) {
 		$("#logo").fadeOut( "fast", function() {
-			$("#logo").removeClass("col-xs-4");
-		$(".navblock").removeClass("col-xs-4");
-		$(".navblock").addClass("col-xs-6");
+			$("#logo").removeClass("col-4");
+		$(".navblock").removeClass("col-4");
+		$(".navblock").addClass("col-6");
   		});
 	} else {
-		$(".navblock").removeClass("col-xs-6");
-		$(".navblock").addClass("col-xs-4");
-		$("#logo").addClass("col-xs-4");
+		$(".navblock").removeClass("col-6");
+		$(".navblock").addClass("col-4");
+		$("#logo").addClass("col-4");
 		setTimeout(function(){
 			if(!$("#biglogo").visible(true))
 				$("#logo").fadeIn("fast");
